5bdd0fe519b7147bed90a39704e82b40f04d7e5d
Maxime Nicole <maxime.nicole@icloud.com>
2015-10-23 17:31:49 +0200

    Fix 404

--- main.js
+++ main.js
@@ -38,13 +38,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-/// catch 404 and forwarding to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
 /// error handlers
 
 // development error handler
